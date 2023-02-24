808aaed5dba32c16fa0ee01ff5d37f8c0e076f24
Amaze0909 <priyanshu.jain@thewitslab.com>
2023-02-24 16:59:35 +0530

    latest error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
 require("@nomicfoundation/hardhat-toolbox");
 require('@nomiclabs/hardhat-truffle5');
-
+require('solidity-coverage');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
